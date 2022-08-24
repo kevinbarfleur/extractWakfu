c545601f4a70f1c3c2592da8f857512017e9503f
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-08-24 10:51:16 +0200

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,8 @@
 const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
-  "https://jwxluouysvjcljgkrfgp.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3eGx1b3V5c3ZqY2xqZ2tyZmdwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjA5ODQzMDUsImV4cCI6MTk3NjU2MDMwNX0.8Sh865N0N3LvywBVRuDyAMggi6aNFBfLLp4a1ex1p9I"
+  "url",
+  "anonkey"
 );
 
 const db = {
