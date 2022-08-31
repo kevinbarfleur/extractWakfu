f18326dc5ee13b43958314b0d18d1a57cf6ce9aa
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-08-31 14:50:04 +0200

    Delete extractWakfu directory

--- extractWakfu/index.js
+++
@@ -1,8 +0,0 @@
-const browserObject = require('./src/browser');
-const scraperController = require('./src/scraper');
-
-//Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
-
-// Pass the browser instance to the scraper controller
-scraperController(browserInstance)
